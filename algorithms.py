102b3a1ebf617536220659001d687604a8891eb9
parkerwatson96 <prw008@bucknell.edu>
2023-01-03 19:13:18 -0800

    added insertion and merge sort

--- algorithms.py
+++ algorithms.py
@@ -35,7 +35,7 @@ def bfs(visited, graph, node):
 
 def bubble_sort(aList):
     '''
-    Sorts aList using bubble sort method. Not recommended for actual use.
+    Sorts aList using bubble sort method + early stopping. Not recommended for actual use.
 
     params:
         - aList: List
@@ -43,12 +43,69 @@ def bubble_sort(aList):
     n = len(aList)
     for i in range(n):
         sorted = True
-        for j in range(n - i - 1):
-            if aList[j] > aList[j + 1]:
-                aList[j], aList[j + 1] = aList[j + 1], aList[j]
+        for j in range(n-i-1):
+            if aList[j] > aList[j+1]:
+                aList[j], aList[j+1] = aList[j+1], aList[j]
                 sorted = False
 
         if sorted:
             break
 
     return aList
+
+def insertion_sort(aList):
+    '''
+    Sorts aList using insertion sort method. Not recommeded for actual use
+
+    params:
+        - aList: List
+    '''
+    for i in range(1, len(aList)):
+        key_item = aList[i]
+        j = i - 1
+
+        while j >= 0 and aList[j] > key_item:
+            aList[j+1] = aList[j]
+            j -= 1
+
+        aList[j+1] = key_item
+
+    return aList
+
+
+def merge_sort(aList):
+    if len(aList) < 2:
+        return aList
+
+    midpoint = len(aList) // 2
+
+    return merge(merge_sort(aList[:midpoint]), merge_sort(aList[midpoint:]))
+
+
+def merge(left, right):
+    if len(left) == 0:
+        return right
+
+    if len(right) == 0:
+        return left
+
+    result = []
+    index_left = index_right = 0
+
+    while len(result) < len(left) + len(right):
+        if left[index_left] <= right[index_right]:
+            result.append(left[index_left])
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
+
+        if index_right == len(right):
+            result += left[index_left:]
+            break
+
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+
+    return result
